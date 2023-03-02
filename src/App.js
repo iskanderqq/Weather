8a0c503a6d2e701b4af2fbc0fb9e951f6cc58bbf
Iskander Nureev <iskander.qq@mail.ru>
2023-03-02 16:11:28 +0300

    com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+function WeatherDate(props){
+    let date = new Date()
+    let a = "asd"
+    return(
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            {date.toLocaleDateString()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    )
+}
+
+function City(props){
+    return(
+        <p>
+            Казань
+        </p>
+    )
+}
+
+// function Temp(props){
+//     async fucn
+//     return(
+//         <p>
+
+//         </p>
+//     )
+// }
+
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            temp: 0,
+        }
+        
+    }
+    async temp(){
+        let asd;
+        let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=55.7823547&lon=49.1242266&appid=1ea38a5fe3d4112ce0838391dcd621e3")
+        console.log(response)
+        asd = await response.json()
+        this.setState({
+            temp: asd.main.temp
+        })
+    }
+
+
+    render(){
+        let tempo =  this.state.temp - 273.15
+        this.temp()
+        return(
+        
+            <div className="wrapper">
+
+                <div className="weather-block">
+                    <div className="weather-block__about">
+                        <WeatherDate/>
+                        <City ></City>
+                    </div>
+                   <div>
+
+                   </div>
+                        <p> {Math.round(tempo)}</p>
+                    </div>
+            </div>
+        )
+    }
 }
 
 export default App;
