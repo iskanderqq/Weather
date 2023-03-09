ba62b5f904258faf94a1187e24e1c8a786f16fe0
Iskander Nureev <iskander.qq@mail.ru>
2023-03-09 06:52:13 +0300

    done fetch requests logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
 function WeatherDate(props){
@@ -15,83 +15,121 @@ function WeatherDate(props){
 function City(props){
     return(
         <p>
-            Казань
+            {props.city}
         </p>
     )
 }
 
 function Temp(props){
-    if(props.error){
+   
+    if(props.err){
         return(
-            <p>{props.error.message}</p>
+            <p>Ошибка</p>
         )
-    }else if(!props.isLoaded){
+    }else if(props.answ === null){
+        return(
+            <p>Загрузка</p>
+        )
+    }else{
+        let t  = props.answ.main.temp - 273.15;
+        let c  = props.answ.weather[0].main;
         return(
             <p>
-                Loading
-            </p>
-        )
-    }else {
-        return(
-            <p>
-                {Math.round(props.res - 273.15) }
+               {Math.round(t)} &deg;, {c}
             </p>
         )
     }
 }
 
+async function getCord(city){
+   
+    let res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=6cc8447dbdf18663729ccee4064bc370
+    `)
+    let answ  = await res.json();
+    return answ;
+}
+
+
+async function getData(lat, lon){
+    let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6cc8447dbdf18663729ccee4064bc370`)
+
+    let answ = await res.json()
+
+    return answ
+}
 
 
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            res: null,
-        };
+function App(){
+
+    
+    const [city, cityChange] = useState()
+    const [err, errChange] = useState()
+    const [answ, answChange] = useState(null)
+    const [text, textChange] = useState()
+
+   useEffect(()=>{
+    errChange(null)
+    let a;
+    let b;
+    if(city !== undefined && city !== null && city !== ""){
+        getCord(city).then((result)=>{
+           a = result[0].lat
+           b = result[0].lon
+           
+        }).then(()=>{
+            getData(a, b).then((result) =>{
+                
+                answChange(result)
+            })
+        }).catch((err)=>{
+            errChange(err)
+        })
+    }
+  
+
+    
+   }, [city])
+
+    function handleChange(ev){
         
+        textChange(ev.target.value)
+    }
+
+    function handleSubmit(ev){
+        cityChange(text);
+        ev.preventDefault()
     }
    
-    componentDidMount(){
-        fetch("https://api.openweathermap.org/data/2.5/weather?lat=55.7823547&lon=49.1242266&appid=6cc8447dbdf18663729ccee4064bc370")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                this.setState({
-                    isLoaded: true,
-                    res: result.main.temp
-                });
-            },
-            (error) =>{
-                this.setState({
-                    isLoaded: true,
-                    error
-                })
-            }
-        )
-    }
-
-    render(){
-      const {error, isLoaded, res} = this.state;
-        return(
+  return(
+    <div className="wrapper">
+    <form className="form" onSubmit={handleSubmit}>
+        <label>
+            Город:
+            <input className="cityInput" value={text || ''} onChange={handleChange}></input>
+        </label>
+    
+       <input type="submit" value="Запрос" className="form-btn"></input> 
+    </form>
         
-            <div className="wrapper">
-
-                <div className="weather-block">
-                    <div className="weather-block__about">
-                        <WeatherDate/>
-                        <City ></City>
-                    </div>
-                   <div>
-
-                   </div>
-                   <Temp error ={error} isLoaded = {isLoaded} res = {res}></Temp>
-                    </div>
-            </div>
-        )
-    }
+    <div className="weather-block">
+        <div className="weather-block__about">
+            <WeatherDate/>
+            <City  city = {city}></City>
+        </div>
+       <div>
+       <Temp answ = {answ} err ={err}></Temp>
+       </div>
+          
+        </div>
+</div>
+  )
 }
+
+
+
+
+
+
 
 export default App;
